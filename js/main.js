4f08197db3172edc846b4329780c35f1f1608da1
ankan17 <friendyankan@gmail.com>
2017-12-05 23:36:17 +0530

    Style the events page

--- js/main.js
+++ js/main.js
@@ -24,12 +24,12 @@ function showEventDescription() {
     });
 }
 
-function smoothScrolling() {
-    $('.carousel-container a[href="#"]').click (function(e) {
+function homePageSmoothScrolling() {
+    $('.carousel-container a[href="#"]').click(function(e) {
         e.preventDefault();
         var target = $('#about-us');
         $('html, body').animate({
-            'scrollTop' : $(target).offset().top
+            'scrollTop' : $(target).offset().top-60
         }, 600);
     });
 }
@@ -42,9 +42,60 @@ function showEventModal() {
     });
 }
 
+function assignEventIds() {
+    $('.event-page .event h2').each(function() {
+        var id = $(this).html().toLowerCase().replace(/\ /g, '-');
+        $(this).parent().attr('id', id);
+    });
+    $('.event-page #sidebar a[href="#"]').each (function() {
+        var id = $(this).html().toLowerCase().replace(/\ /g, '-');
+        $(this).attr('href', '#'+id);
+    });
+    $('.event-page #sidebar a[href^="#"]').click(function(e) {
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            'scrollTop' : $(target).offset().top-60
+        }, 600);
+    });
+}
+
+function eventPageSmoothScrolling() {
+    $('.navbar .dropdown-menu a[href="#"]').each(function() {
+        var id = $(this).html().toLowerCase().replace(/\ /g, '-');
+        $(this).attr('href', '#'+id);
+    });
+    $('.navbar .dropdown-menu a[href^="#"]').click(function(e) {
+        var target = $(this).attr('href');
+        $('.navbar-toggle').trigger("click");
+        $('html, body').animate({
+            'scrollTop' : $(target).offset().top-60
+        }, 600);
+    });
+}
+
+function handleShowEventDesc() {
+    $('.show-event-desc').click(function(e) {
+        if ($(this).html() == "Show more") {
+            $(this).siblings($('.event-desc')).addClass('show-text');
+            $(this).html("Show less");
+        }
+        else {
+            $(this).siblings($('.event-desc')).removeClass('show-text');
+            $(this).html("Show more");
+        }
+    });
+}
+
 $(document).ready(function() {
+    // Stop browser from appending # when clicking empty links
+    $('a[href="#"]').click(function(e) {
+        e.preventDefault();
+    });
     applyStickyNavbar();
     showEventDescription();
-    smoothScrolling();
+    homePageSmoothScrolling();
     showEventModal();
+    assignEventIds();
+    eventPageSmoothScrolling();
+    handleShowEventDesc();
 });
